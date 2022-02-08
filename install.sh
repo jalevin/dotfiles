f512708af53591285a42935eac02bba937dd9789
Jeff <jeff@Jeffs-Code-Beast.local>
2022-02-08 13:04:31 -0900

    rework brew config

--- install.sh
+++ install.sh
@@ -1,13 +1,35 @@
 #! /bin/sh
 
+# exit if we get an error
+set -e
+
 # call this from $HOME/{projects}/dotfiles
 # assumes you've git cloned dotfiles and are auth'd to github
+
+ln -F -s "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -F -s "$PWD/zshrc" "$HOME/.zshrc"
+ln -F -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+ln -F -s "$PWD/gemrc" "$HOME/.gemrc"
+
+# export brews - documentation. never used in this script.
+# think about adding to .zshrc
+#brew leaves > brews_all.txt
 
 #brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# minimal brew install
-brew install git neovim@HEAD
+# make sure we have important shell variables
+source "$HOME/.zshrc"
+
+# install developer tooling
+xargs brew install < "$PWD/brews/essential.txt"
+
+# install gui apps
+xargs brew install < "$PWD/brews/gui.txt"
+
+# install optional # FIXME - trim this list
+#xargs brew install "$PWD/brews/extra.txt"
+
 
 #autoload
 AUTOLOAD="$HOME/vim/autoload"
@@ -20,29 +42,3 @@ ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
 ln -s "$PWD/vim.vimrc" "$HOME/.vim.vimrc"
 ln -s "$PWD/default.vimrc" "$HOME/.vimrc"
 ln -s "$PWD/vscode.vimrc" "$HOME/.vscode.vimrc"
-
-ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
-ln -s "$PWD/zshrc" "$HOME/.zshrc"
-ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
-ln -s "$PWD/gemrc" "$HOME/.gemrc"
-
-
-
-
-
-
-
-#neovim
-#NVIM v0.6.0-dev+434-g7f93b2ab0
-#brew install neovim --HEAD
-
-#brew install \
-  #the_silver_searcher \
-  #rbenv \
-  #pyenv \
-  #ripgrep \
-  #vgrep \
-  #jq \
-  #tldr
-
-# go
